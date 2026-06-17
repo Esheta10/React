494bec82ff83942c04f97b60c8abf3e6c669838f
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-06-17 13:30:46 +0530

    Inline Styles

---
+++ 10-styling-react-components/src/Button.jsx
@@ -0,0 +1,12 @@
+export const Button = () => {
+    return (
+        <button style = {
+            {
+                backgroundColor: 'blue',
+                color: 'white',
+                padding: '10px 20px',
+                borderRadius: '5px'
+            }
+        }>Click Me</button>
+    );
+}
