213c8ab9f3a98d9c302c1ef2ff5b8d6ecb7b74f6
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-06-17 11:57:28 +0530

    conditional rendering using ternary operator

--- 07-conditional-rendering/src/UserDetails.jsx
+++ 07-conditional-rendering/src/UserDetails.jsx
@@ -1,20 +1,30 @@
+// if- statement
+// export const UserDetails = ({name, isOnline}) => {
+//    if(isOnline){
+//         return(
+//             <div>
+//                 <h3>{name}</h3>
+//                 <span>Online</span>
+//                 <p>Available for chat</p>
+//                 <button>Send message</button>
+//             </div>
+//         );
+//    }
+//    return(
+//         <div>
+//             <h3>{name}</h3>
+//             <span>Offline</span>
+//             <p>Not available</p>
+//             <small>Check back later</small>
+//         </div>
+//    );
+// }
+
+// ternary operator
 export const UserDetails = ({name, isOnline}) => {
-   if(isOnline){
-        return(
-            <div>
-                <h3>{name}</h3>
-                <span>Online</span>
-                <p>Available for chat</p>
-                <button>Send message</button>
-            </div>
-        );
-   }
-   return(
+    return(
         <div>
-            <h3>{name}</h3>
-            <span>Offline</span>
-            <p>Not available</p>
-            <small>Check back later</small>
+            {isOnline ? <h3>Welcome back! {name}</h3> : <h3>Please sign in</h3>}
         </div>
-   );
+    );
 }
