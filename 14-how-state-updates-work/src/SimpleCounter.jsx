6be79695bb1824b0e41317a35c5e09c61f278867
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-06-18 13:40:52 +0530

    Throughout the trigger phase, we are only queueing updates and only in the render phase does the count actually increment

--- 14-how-state-updates-work/src/SimpleCounter.jsx
+++ 14-how-state-updates-work/src/SimpleCounter.jsx
@@ -7,9 +7,15 @@ export const SimpleCounter = () => {
     console.log("Render phase: Component Rendering with count: ", count);
 
     const handleClick = () => {
-        console.log("Before setCount, count is: ", count);
+      
         setCount(count+1);
-        console.log("Still in trigger phase, after setCount, count is: ", count);
+        console.log("After setCount(count + 1), count is: ", count);
+
+        setCount(count+5);
+        console.log("After setCount(count + 5), count is: ", count);
+
+        setCount(count+10);
+        console.log("After setCount(count + 10), count is: ", count);
     }
 
     return (
