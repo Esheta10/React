26e370fb48704b35c561cf8f7767cf6048ec4251
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-06-18 13:25:52 +0530

    event handlers don't update state in trigger phase

--- 14-how-state-updates-work/src/SimpleCounter.jsx
+++ 14-how-state-updates-work/src/SimpleCounter.jsx
@@ -2,14 +2,20 @@ import {useState} from "react"
 
 export const SimpleCounter = () => {
 
-    console.log("Simple Counter Component Rendered");
-    
     const [count, setCount] = useState(0);
+
+    console.log("Render phase: Component Rendering with count: ", count);
+
+    const handleClick = () => {
+        console.log("Before setCount, count is: ", count);
+        setCount(count+1);
+        console.log("Still in trigger phase, after setCount, count is: ", count);
+    }
 
     return (
       <div>
         <div>Count : {count}</div>
-        <button onClick={() => {setCount(count+1)}}>Increment</button>
+        <button onClick={handleClick}>Increment</button>
       </div>
     );
 }
