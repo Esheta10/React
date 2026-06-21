961c40dc26412c53d6da9478b2a403d9cd1cc470
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-06-22 00:19:28 +0530

    Fetching data with useState and useEffect

---
+++ 24-fetching-data-with-useReducer/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {DataFetchingOne} from './DataFetchingOne'
+
+const App = () => {
+  return (
+    <div>
+      <DataFetchingOne/>
+    </div>
+  )
+}
+
+export default App
