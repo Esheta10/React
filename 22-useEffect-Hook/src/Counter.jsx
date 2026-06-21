e8f3c62114c0a67266bfb6663d1c2c731ca5a7aa
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-06-21 11:28:07 +0530

    conditionally run useEffect

--- 22-useEffect-Hook/src/Counter.jsx
+++ 22-useEffect-Hook/src/Counter.jsx
@@ -4,12 +4,16 @@ import {useEffect} from "react"
 export const Counter = () => {
 
     const [count, setCount] = useState(0);
+    const [name, setName] = useState(0);
+
 
     useEffect(() => {
+        console.log("Updating - document title");
         document.title = `You clicked ${count} times`
-    })
+    },[count])
     return (
         <div>
+            <input type="text" value={name} onChange = {e => setName(e.target.value)} />
             <button onClick={() => setCount(count+1)}>Count: {count}</button>
         </div>
     );
