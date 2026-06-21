6c9367fbbd11b565623ba4a8ad6408a48d168f85
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-06-21 17:56:33 +0530

    Data Fetching with Post Id on button click

--- 23-fetching-data-with-useEffect/src/DataFetchingWithId.jsx
+++ 23-fetching-data-with-useEffect/src/DataFetchingWithId.jsx
@@ -5,19 +5,25 @@ export const DataFetchingWithId = () => {
 
     const [post, setPost] = useState({}) ; // empty object for individual post
     const [id, setId] = useState(1);
+    const[idFromButton, setIdFromButton] = useState(1);
 
     
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButton}`)
         .then(res => {
         console.log(res);
         setPost(res.data);
         })
         .catch(err => console.log(err));
-    }, [id]);
+    }, [idFromButton]);
+
+    const handleClick = () => {
+        setIdFromButton(id);
+    }
     return (
         <div>
             <input type="text" value={id} onChange={(e) => setId(e.target.value)}/>
+            <button type="button" onClick={handleClick}>Fetch Post</button>
             <div>{post.title}</div>
         </div>  
     );
