ac2e2c1bf347041045f533d4a882a4bc69d1b91e
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-06-22 15:22:36 +0530

    Search Params

--- 25-react-router-demo/src/UserDetails.jsx
+++ 25-react-router-demo/src/UserDetails.jsx
@@ -1,7 +1,13 @@
+import { useParams } from "react-router-dom";
+
 export const UserDetails = () => {
+
+    const params = useParams();
+    const userId = params.userId;
+
     return (
         <div>
-            <h4>Details about User</h4>
+            <h4>Details about User : {userId}</h4>
         </div>
     );
 }
--- 25-react-router-demo/src/Users.jsx
+++ 25-react-router-demo/src/Users.jsx
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 export const Users = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const showActiveUsers = searchParams.get('filter') === "active"
+
     return (
         <div>
             <h1>Users</h1>
             <Outlet/> {/*to render child components */}
+            <div>
+                <button onClick={() => setSearchParams({filter : 'active'})}>Active Users</button>
+                <button onClick={() => setSearchParams({})}>Reset Filter</button>
+            </div>
+            {
+                showActiveUsers ? (<h2>Showing active users</h2>) : (<h2>Showing all users</h2>)
+            }
         </div>
     );
 }
